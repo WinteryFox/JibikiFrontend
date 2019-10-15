95b441bbcef2058dbf7b13efac15938b3a8dfbd1
WinteryFox <dragonhel5@gmail.com>
2019-10-15 20:09:25 +0200

    basic search

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/search.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Search
   }
 }
 </script>
