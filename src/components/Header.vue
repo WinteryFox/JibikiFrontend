8be03a3b5d286818fe1f44fada52bc32bbdcffee
WinteryFox <dragonhel5@gmail.com>
2019-10-20 15:41:48 +0200

    rework a little

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,35 @@
 <template>
-    <div class="container">
-        <h1>Jibiki</h1>
-    </div>
+    <header class="header">
+        <h1><a class="stop" href="/">Jibiki</a></h1>
+        <nav>
+            <ul>
+                <li>
+                    <a class="stop" target="_blank" href="https://discord.gg/aKTwN5c">Discord</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
+
+<style scoped>
+    nav {
+
+    }
+
+    ul {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+    }
+
+    .stop {
+        color: #b9b9b9;
+        text-decoration: none;
+    }
+</style>
 
 <script>
     export default {
         name: "Header"
     }
 </script>
-
-<style scoped>
-
-</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -7,40 +7,97 @@
                        v-on:keyup="query(search)"/>
             </label>
         </div>
-        <div class="container">
-            <div v-bind:id="word.id" v-bind:kanj="word.kanj" class="word" v-for="word in words">
-                <div class="container entry">
-                    <div class="container">
-                        <span class="container" id="furigana" :key="word.readings[0]">{{word.readings[0]}}</span>
-                        <span class="container" id="kanji">{{word.kanji}}</span>
+        <div>
+            <transition-group name="word" tag="div">
+                <div v-for="word in words" v-bind:key="word" class="word">
+                    <div class="entry">
+                        <div>
+                            <div id="furigana">{{word.forms[0].reading}}</div>
+                            <div id="kanji">{{word.forms[0].kanji}}</div>
+                        </div>
                     </div>
-                    <div class="container" v-if="word.readings.length > 1">
-                        <span>Other forms</span>
-                        <span class="container" :key="reading" v-for="(reading, i) in word.readings"
-                              v-if="i > 0">{{reading}}</span>
+                    <div class="senses">
+                        <div class="sens" :key="sens" v-for="(sens, i) in word.senses">
+                            <div class="posses">
+                                <span v-for="pos in sens.pos" class="pos">{{pos}}</span>
+                            </div>
+                            <div class="flds">
+                                <div v-for="fld in sens.fld" class="fld">{{fld}}</div>
+                            </div>
+                            <div>
+                                <div>{{i + 1}}. {{sens.gloss.join('; ')}}</div>
+                                <div class="note">{{sens.notes}}</div>
+                                <div class="note">{{sens.misc.join('; ')}}</div>
+                            </div>
+                        </div>
+                        <div class="forms small">
+                            <div v-if="word.forms.length > 1">Other forms</div>
+                            <div v-for="(form, i) in word.forms" v-if="i > 0">
+                                <div>
+                                    {{form.kanji}} <span class="note">{{form.kanji_info}}</span>
+                                </div>
+                                <div>
+                                    {{form.reading}} <span class="note">{{form.reading_info}}</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="container sens" :key="sens.sens" v-for="(sens, i) in word.senses">
-                    <div>
-                        <span :key="pos" v-for="pos in sens.pos">{{pos}}, </span>
-                    </div>
-                    <div>
-                        <span :key="fld" v-for="fld in sens.fld">{{fld}}, </span>
-                    </div>
-                    <div>
-                        <span>{{i + 1}}. </span>
-                        <span :key="gloss"
-                              v-for="gloss in sens.gloss">{{gloss}}; </span>
-                    </div>
-                </div>
-            </div>
+            </transition-group>
         </div>
     </div>
 </template>
 
 <style scoped>
+    .small {
+        font-size: 14px;
+    }
+
+    .forms {
+        padding-top: 6px;
+    }
+
+    .posses {
+        margin-bottom: 6px;
+    }
+
+    .flds {
+        margin-bottom: 6px;
+    }
+
+    .pos {
+        display: inline-block;
+        background-color: #ff7783;
+        border-radius: 3px;
+        padding: 1px 10px;
+        font-size: 12px;
+        color: #545454;
+    }
+
+    .fld {
+        display: inline-block;
+        background-color: #f666ff;
+        border-radius: 3px;
+        padding: 1px 10px;
+        font-size: 12px;
+        color: #545454;
+    }
+
+    .word-enter-active, .word-leave-active {
+        transition: all .5s;
+    }
+
+    .word-enter, .word-leave-to {
+        opacity: 0;
+        transform: translateX(-30px);
+    }
+
+    .word-leave, .word-leave-to {
+        transform: translateX(30px);
+    }
+
     .search {
-        margin: 0 auto;
+        margin: 0 auto 30px;
         width: 80%;
         position: relative;
         border-radius: 8px;
@@ -52,16 +109,13 @@
         background: #b9b9b9 url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjNjY2IiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCI+PGNpcmNsZSBjeD0iMTEiIGN5PSIxMSIgcj0iOCIvPjxwYXRoIGQ9Ik0yMSAyMWwtNC00Ii8+PC9zdmc+") no-repeat 12px 12px;
     }
 
+    .note {
+        font-size: 14px;
+        color: #808080
+    }
+
     .entry {
         float: left;
-    }
-
-    .sens {
-        text-align: center;
-    }
-
-    .word {
-        margin: 40px;
     }
 
     #furigana {
@@ -71,8 +125,33 @@
     #kanji {
         font-size: 36px;
     }
-</style>
 
+    .word {
+        width: 80%;
+        display: flex;
+        flex-wrap: nowrap;
+        flex-direction: row;
+        align-content: center;
+        align-items: flex-start;
+        justify-content: center;
+        padding: 16px;
+        margin: 0 auto 16px;
+        border-bottom: 1px solid gray;
+    }
+
+    .word .entry {
+        text-align: left;
+        display: inline-block;
+        width: 150px;
+        max-width: 33.333%;
+    }
+
+    .word .senses {
+        text-align: left;
+        display: block;
+        width: calc(100% - 125px);
+    }
+</style>
 
 <script>
     import axios from 'axios'
