f29a75ce063a05083e1db4dcb8a797e8147ea20a
WinteryFox <dragonhel5@gmail.com>
2019-11-07 20:36:29 +0100

    documentation color palette

--- src/components/KanjiSearch.vue
+++ src/components/KanjiSearch.vue
@@ -1,5 +1,5 @@
 <template>
-    <md-content class="md-elevation-3">
+    <div class="root">
         <md-field md-inline>
             <label>
                 <md-icon>search</md-icon>
@@ -10,39 +10,51 @@
                       v-on:keyup="getKanji(search)" autofocus>
             </md-input>
         </md-field>
-        <div class="md-layout" v-for="kanji in this.kanji">
-            <div class="md-layout-item kanji">
-                {{kanji.literal}}
-            </div>
-            <div class="md-layout-item">
-                <div>
-                    <md-chip v-if="kanji.grade != null" class="md-raised md-primary">
-                        Grade {{kanji.grade}}
-                    </md-chip>
-                    <md-chip v-if="kanji.jlpt != null" class="md-raised md-accent">
-                        JLPT {{kanji.jlpt}}
-                    </md-chip>
+        <md-content class="md-elevation-3" :key="kanji.literal" v-for="kanji in this.kanji">
+            <div class="md-layout">
+                <div class="md-layout-item">
+                    <h1>{{kanji.literal}}</h1>
+                    <div class="chip">
+                        <md-chip v-if="kanji.grade != null" class="md-raised md-primary">
+                            Grade {{kanji.grade}}
+                        </md-chip>
+                    </div>
+                    <div class="chip">
+                        <md-chip v-if="kanji.jlpt != null" class="md-raised md-accent">
+                            JLPT N{{kanji.jlpt}}
+                        </md-chip>
+                    </div>
                 </div>
-                <p v-for="(meaning, i) in kanji.meaning">{{i + 1}}. {{meaning}}</p>
-            </div>
-            <div class="md-layout-item">
-                <div>
-                    <p v-if="kanji.kunyomi.length > 0">Kunyomi</p>
-                    <p>{{kanji.kunyomi.join(', ')}}</p>
+                <div class="md-layout-item">
+                    <p v-for="(meaning, i) in kanji.meaning">{{i + 1}}. {{meaning}}</p>
                 </div>
-                <div>
-                    <p v-if="kanji.onyomi.length > 0">Onyomi</p>
-                    <p>{{kanji.onyomi.join(', ')}}</p>
+                <div class="md-layout-item">
+                    <div>
+                        <p v-if="kanji.kunyomi.length > 0">Kunyomi</p>
+                        <p>{{kanji.kunyomi.join(', ')}}</p>
+                    </div>
+                    <div>
+                        <p v-if="kanji.onyomi.length > 0">Onyomi</p>
+                        <p>{{kanji.onyomi.join(', ')}}</p>
+                    </div>
                 </div>
             </div>
             <md-divider/>
-        </div>
-    </md-content>
+        </md-content>
+    </div>
 </template>
 
 <style scoped lang="scss">
     .kanji {
         font-size: 36px;
+    }
+
+    .chip {
+        padding-top: 10px;
+    }
+
+    .md-layout {
+        margin: 30px;
     }
 </style>
 
