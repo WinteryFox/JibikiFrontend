18b1f7dad7dba577b0537c7ee030b80766bde98b
WinteryFox <dragonhel5@gmail.com>
2019-11-12 20:05:35 +0100

    add github icon and move discord tab into icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,10 +6,14 @@
             <md-tab id="tab-words" md-label="Words" to="/words" md-icon="translate"/>
             <md-tab id="tab-kanji" md-label="Kanji" to="/kanji" md-icon="/kanji.svg"/>
             <md-tab id="tab-documentation" md-label="API Docs" to="/docs" md-icon="library_books"/>
-            <md-tab id="tab-discord" md-label="Discord" href="https://discord.gg/aKTwN5c"
-                    md-icon="https://discordapp.com/assets/f8389ca1a741a115313bede9ac02e2c0.svg"/>
         </md-tabs>
-        <md-button class="md-icon-button" v-on:click="changeTheme">
+        <md-button class="md-icon-button discord" href="https://discord.gg/aKTwN5c" target="_blank">
+            <md-icon md-src="https://discordapp.com/assets/f8389ca1a741a115313bede9ac02e2c0.svg"/>
+        </md-button>
+        <md-button class="md-icon-button github" href="https://github.com/WinteryFox" target="_blank">
+            <md-icon md-src="/github.svg"/>
+        </md-button>
+        <md-button class="md-icon-button theme" v-on:click="changeTheme">
             <md-icon>invert_colors</md-icon>
         </md-button>
     </div>
@@ -35,9 +39,20 @@
         font-size: 40px;
     }
 
+    .theme {
+        right: 25px;
+    }
+
+    .github {
+        right: 70px;
+    }
+
+    .discord {
+        right: 115px;
+    }
+
     .md-button {
         position: absolute;
-        right: 25px;
         z-index: 2;
         top: 50%;
         transform: translate(0, -50%);
