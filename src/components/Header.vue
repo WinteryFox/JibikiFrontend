80f4a32e00e3a7af65b69b8c75a32b83b3abccbc
WinteryFox <dragonhel5@gmail.com>
2019-12-03 21:05:29 +0100

    remove tabs from Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -44,10 +44,6 @@
                 </md-button>
             </div>
         </md-content>
-        <md-tabs md-alignment="centered" md-sync-route>
-            <md-tab id="tab-home" md-label="Home" to="/" md-icon="home" exact/>
-            <md-tab id="tab-documentation" md-label="Developer" to="/docs" md-icon="library_books"/>
-        </md-tabs>
         <div class="login">
             <md-dialog :md-active.sync="showLogin">
                 <md-dialog-title>
@@ -131,10 +127,6 @@
 <style scoped lang="scss">
     @import '~vue-material/dist/theme/engine';
 
-    .login {
-
-    }
-
     .header {
         position: relative;
 
@@ -142,49 +134,15 @@
             line-height: normal;
         }
 
-        .text {
-            top: 10px;
-            width: 100px;
-        }
-
-        .md-content {
-            top: 35px;
-        }
-
-        @media screen and (max-width: 768px) {
-            .md-button {
-                margin: 1px;
-            }
-
-            .md-content {
-                top: 0;
-                height: 55px;
-            }
-
-            .md-tabs {
-                position: fixed;
-                bottom: 0;
-                width: 100%;
-                z-index: 2;
-            }
-
-            .text {
-                display: none;
-            }
-        }
-
         h3 {
             color: md-get-palette-color(pink, 200);
-            position: absolute;
-            z-index: 2;
-            padding-left: 25px;
             font-size: 40px;
-            transform: translate(0, -60%);
+            margin: 0;
+            padding: 10px 40px;
         }
 
         .icons {
             position: absolute;
-            z-index: 2;
             top: 50%;
             transform: translate(0, -50%);
             right: 0;
@@ -192,6 +150,20 @@
 
         small {
             font-size: 20px;
+        }
+
+        @media screen and (max-width: 768px) {
+            .md-button {
+                margin: 1px;
+            }
+
+            h3 {
+                padding: 10px 20px;
+            }
+
+            .text {
+                display: none;
+            }
         }
     }
 </style>
